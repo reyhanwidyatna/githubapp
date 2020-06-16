aa0a659114dad202f83982225f003761e1276661
reyhanwidyatna <reyhan_19@apps.ipb.ac.id>
2020-06-16 09:41:23 +0700

    [fix notification]

--- app/src/main/java/com/example/dicodingapp/NotificationReceiver.java
+++ app/src/main/java/com/example/dicodingapp/NotificationReceiver.java
@@ -1,6 +1,7 @@
 package com.example.dicodingapp;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,15 +14,17 @@ public class NotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Intent repeating = new Intent(context, MainActivity.class);
-        repeating.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_add_alert_black_24dp)
                 .setContentTitle("Daily Reminder")
                 .setContentText("Please open your app")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setAutoCancel(true);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(contentIntent);
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(NOTIFICATION_ID, builder.build());
